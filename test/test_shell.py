ae30debbea40b485d66ca61eb7fd4227f0b41d65
aadi singhi <aadi.singhi.21@ucl.ac.uk>
2022-10-28 06:24:54 +0100

    "Added cd and Head"

--- test/test_shell.py
+++ test/test_shell.py
@@ -2,6 +2,7 @@ import unittest
 
 from shell import eval
 from collections import deque
+import os
 
 
 class TestShell(unittest.TestCase):
@@ -10,6 +11,26 @@ class TestShell(unittest.TestCase):
         eval("echo foo", out)
         self.assertEqual(out.popleft(), "foo\n")
         self.assertEqual(len(out), 0)
+
+    def testCd(self):
+        out = deque()
+        #print('testing cd')
+        args = 'COMP0010'
+        c = cd.Cd()
+        c.exec(args,None,out)
+        #self.assertIsNotNone(args)
+        self.assertEqual(out,'COMP0010')
+
+    def testHead(self):
+        os.chdir('COMP0010')
+        out = deque()
+        current_dir = os.getcwd()
+        args = ["requirements.txt"]
+        h = head.Head()
+        h.exec(args, None, out)
+        #self.assertIsNotNone(current_dir)
+        data = "nose2\ncoverage\nflake8==3.8.0\nflake8-html\njunit2html"
+        self.assertEqual(out, data)
 
 
 if __name__ == "__main__":
