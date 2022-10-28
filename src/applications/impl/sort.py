c5f432a069df7bc66d242f9b34601738da4e33c9
prashamjadhwani <prasham.jadhwani.21@ucl.ac.uk>
2022-10-28 15:36:35 +0100

    added sort functionality

--- src/applications/impl/sort.py
+++ src/applications/impl/sort.py
@@ -5,4 +5,48 @@ from typing import Optional
 
 class Sort(Application):
     def exec(self, args: list, input_: Optional[list], out: deque) -> None:
-        pass
+        if len(args)==0 and input_ is None:
+            raise ValueError("wrong number of arguments")
+
+        if len(args)==1 and args[0]=="-r" and input_ is None:
+            raise ValueError("wrong number of arguments")
+
+        if input_ is not None:
+            if len(args)==1 and args[0]=="-r":
+                args.pop(0)
+                args.extend(input_)
+                args.sort(reverse=True)
+
+            else:
+                args.extend(input_)
+                args.sort()
+
+        else:
+            if args[0]=="-r":
+                filename=args[1]
+
+                result=self.sort_file_contents(filename,True)
+                args.pop(0)
+                args.pop(0)
+                args.extend(result)
+            else:
+                filename=args[0]
+                args.pop(0)
+                result=self.sort_file_contents(filename)
+                args.extend(result)
+
+        for arg in args:
+            out.append(arg)
+
+
+    def sort_file_contents(self,filename,reversed=False):
+        file_content=[]
+        with open(filename) as f:
+            lines=f.readlines()
+            for line in lines:
+                file_content.append(line)
+
+        file_content.sort(reverse=reversed)
+        return file_content
+
+
--- test/test_shell.py
+++ test/test_shell.py
@@ -20,11 +20,32 @@ class TestShell(unittest.TestCase):
 
     def testGrep(self):
         out = deque()
-        
         eval("grep he test",out)
         self.assertEqual(out.popleft(), "hello\n")
         self.assertEqual(len(out), 0)
 
+    def testGrep_multipleFiles(self):
+        out=deque()
+        eval("grep he test2 test",out)
+        result=list(out)
+        self.assertEqual(result,["test2:hehehehe\n","test2:hellohello\n","test:hello\n"])
+
+    def testGrep_noMatches(self):
+        out=deque()
+        eval("grep aa test",out)
+        self.assertEqual(len(out),0)
+
+    def test_sort(self):
+        out=deque()
+        eval("sort test2",out)
+        result=list(out)
+        self.assertEqual(result,["aaa\n","hello world\n"])
+
+    def test_sort_r(self):
+        out=deque()
+        eval("sort -r test2",out)
+        result=list(out)
+        self.assertEqual(result,["hello world\n","aaa\n"])
 
 
 
