6ed60a6451c9d9752933d19463b9531feb1baed2
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-10-28 12:22:50 +0100

    Fix shell.py

--- src/shell.py
+++ src/shell.py
@@ -1,12 +1,12 @@
 import re
 import sys
 import os
-from os import listdir
 from collections import deque
 from glob import glob
+from applications.application_factory import ApplicationFactory
 
 
-def eval(cmdline, out):
+def eval(cmdline: str, out: deque) -> None:
     raw_commands = []
     for m in re.finditer("([^\"';]+|\"[^\"]*\"|'[^']*')", cmdline):
         if m.group(0):
@@ -23,79 +23,66 @@ def eval(cmdline, out):
                     tokens.extend(globbing)
                 else:
                     tokens.append(m.group(0))
-        app = tokens[0]
+        app = ApplicationFactory.by_name(tokens[0])
         args = tokens[1:]
-        if app == "pwd":
-            out.append(os.getcwd())
-        elif app == "cd":
-            if len(args) == 0 or len(args) > 1:
-                raise ValueError("wrong number of command line arguments")
-            os.chdir(args[0])
-        elif app == "echo":
-            out.append(" ".join(args) + "\n")
-        elif app == "ls":
-            if len(args) == 0:
-                ls_dir = os.getcwd()
-            elif len(args) > 1:
-                raise ValueError("wrong number of command line arguments")
-            else:
-                ls_dir = args[0]
-            for f in listdir(ls_dir):
-                if not f.startswith("."):
-                    out.append(f + "\n")
-        elif app == "cat":
-            for a in args:
-                with open(a) as f:
-                    out.append(f.read())
-        elif app == "head":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                for i in range(0, min(len(lines), num_lines)):
-                    out.append(lines[i])
-        elif app == "tail":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                display_length = min(len(lines), num_lines)
-                for i in range(0, display_length):
-                    out.append(lines[len(lines) - display_length + i])
-        elif app == "grep":
-            if len(args) < 2:
-                raise ValueError("wrong number of command line arguments")
-            pattern = args[0]
-            files = args[1:]
-            for file in files:
-                with open(file) as f:
-                    lines = f.readlines()
-                    for line in lines:
-                        if re.match(pattern, line):
-                            if len(files) > 1:
-                                out.append(f"{file}:{line}")
-                            else:
-                                out.append(line)
-        else:
-            raise ValueError(f"unsupported application {app}")
+        app.exec(args, None, out)
+        # if app == "pwd":
+        #     out.append(os.getcwd())
+        # elif app == "cd":
+        #     if len(args) == 0 or len(args) > 1:
+        #         raise ValueError("wrong number of command line arguments")
+        #     os.chdir(args[0])
+        # elif app == "echo":
+        #     out.append(" ".join(args) + "\n")
+        # elif app == "head":
+        #     if len(args) != 1 and len(args) != 3:
+        #         raise ValueError("wrong number of command line arguments")
+        #     if len(args) == 1:
+        #         num_lines = 10
+        #         file = args[0]
+        #     if len(args) == 3:
+        #         if args[0] != "-n":
+        #             raise ValueError("wrong flags")
+        #         else:
+        #             num_lines = int(args[1])
+        #             file = args[2]
+        #     with open(file) as f:
+        #         lines = f.readlines()
+        #         for i in range(0, min(len(lines), num_lines)):
+        #             out.append(lines[i])
+        # elif app == "tail":
+        #     if len(args) != 1 and len(args) != 3:
+        #         raise ValueError("wrong number of command line arguments")
+        #     if len(args) == 1:
+        #         num_lines = 10
+        #         file = args[0]
+        #     if len(args) == 3:
+        #         if args[0] != "-n":
+        #             raise ValueError("wrong flags")
+        #         else:
+        #             num_lines = int(args[1])
+        #             file = args[2]
+        #     with open(file) as f:
+        #         lines = f.readlines()
+        #         display_length = min(len(lines), num_lines)
+        #         for i in range(0, display_length):
+        #             out.append(lines[len(lines) - display_length + i])
+        # elif app == "grep":
+        #     if len(args) < 2:
+        #         raise ValueError("wrong number of command line arguments")
+        #     pattern = args[0]
+        #     files = args[1:]
+        #     for file in files:
+        #         with open(file) as f:
+        #             lines = f.readlines()
+        #             for line in lines:
+        #                 if re.match(pattern, line):
+        #                     if len(files) > 1:
+        #                         out.append(f"{file}:{line}")
+        #                     else:
+        #                         out.append(line)
+        # else:
+        #     raise ValueError(f"unsupported application {app}")
 
 
 if __name__ == "__main__":
