a3319a17b28194366838d6c2363285ad49dd3840
prashamjadhwani <prasham.jadhwani.21@ucl.ac.uk>
2022-10-30 10:31:36 +0000

    updated sort,uniq for stdin

--- src/applications/impl/uniq.py
+++ src/applications/impl/uniq.py
@@ -5,8 +5,9 @@ from typing import Optional
 
 class Uniq(Application):
     def exec(self, args: list, input_: Optional[str], out: deque) -> None:
-        filename = ""
         ignore_case = False
+        result=[]
+
         if len(args) == 0 and input_ is None:
             raise ValueError("wrong number of arguments")
 
@@ -14,40 +15,48 @@ class Uniq(Application):
             raise ValueError("wrong number of arguments")
 
         if input_ is not None:
+            if len(args)==1 and args[0]!="-i":
+                raise ValueError("wrong flag provided")
+
             if len(args)==1 and args[0]=='-i':
                 ignore_case=True
                 args.pop(0)
 
-            filename=input_
-
-        elif len(args) == 2 and args[0] == "-i":
-            filename = args[1]
-            ignore_case = True
-            args.pop(0)
-            args.pop(0)
+            result=[i + "\n" for i in input_.split("\n")]
 
         else:
-            filename = args[0]
-            args.pop(0)
+            filename=""
+            if len(args) == 2 and args[0]!="-i":
+                raise ValueError("wrong flag provided")
 
-        result = self.remove_duplicate_filecontents(filename, ignore_case)
+            if len(args) == 2 and args[0] == "-i":
+                filename = args[1]
+                ignore_case = True
+                args.pop(0)
+                args.pop(0)
+
+            else:
+                filename=args[0]
+                args.pop(0)
+
+            with open(filename) as f:
+                result = self.remove_duplicate_filecontents(f.readlines(), ignore_case)
+
         args.extend(result)
 
         for arg in args:
             out.append(arg)
 
-    def remove_duplicate_filecontents(self, filename, ignore_case=False):
+    def remove_duplicate_filecontents(self, lines, ignore_case=False):
         prev = ""
         result = []
-        with open(filename) as f:
-            lines = f.readlines()
-            for line in lines:
-                if ignore_case:
-                    if not prev.casefold() == line.casefold():
-                        result.append(line)
-                else:
-                    if not prev == line:
-                        result.append(line)
-                prev = line
+        for line in lines:
+            if ignore_case:
+                if not prev.casefold() == line.casefold():
+                    result.append(line)
+            else:
+                if not prev == line:
+                    result.append(line)
+            prev = line
 
         return result
