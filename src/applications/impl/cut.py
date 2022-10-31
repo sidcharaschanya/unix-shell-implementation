218785d06c2e2535cdaa21e233b2f69eda1d72ac
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-10-31 13:13:05 +0000

    Change cut bytes to list

--- src/applications/impl/cut.py
+++ src/applications/impl/cut.py
@@ -11,17 +11,17 @@ class Cut(Application):
         if args[0] != "-b":
             raise ValueError("wrong flags")
 
-        cut_bytes_string, lines = Cut.get_cut_bytes_string_and_lines(args, input_)
+        lines, cut_bytes = Cut.get_lines(args, input_), args[1].split(",")
 
     @staticmethod
-    def get_cut_bytes_string_and_lines(args: list, input_: Optional[str]) -> tuple:
+    def get_lines(args: list, input_: Optional[str]) -> list:
         if len(args) == 2:
             if not input_:
                 raise ValueError("stdin not provided")
 
-            cut_bytes_string, lines = args[1], [i + "\n" for i in input_.split("\n")]
+            lines = [i + "\n" for i in input_.split("\n")]
         else:
             with open(args[2]) as file:
-                cut_bytes_string, lines = args[1], file.readlines()
+                lines = file.readlines()
 
-        return cut_bytes_string, lines
+        return lines
