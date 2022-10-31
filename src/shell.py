0a68c3d37e60ca0fbf4a15af1f9a95e812213047
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-10-31 22:27:14 +0000

    Organize shell.py

--- src/shell.py
+++ src/shell.py
@@ -8,11 +8,14 @@ from applications.application_factory import ApplicationFactory
 
 def eval(cmdline: str, out: deque) -> None:
     raw_commands = []
+
     for m in re.finditer("([^\"';]+|\"[^\"]*\"|'[^']*')", cmdline):
         if m.group(0):
             raw_commands.append(m.group(0))
+
     for command in raw_commands:
         tokens = []
+
         for m in re.finditer("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'", command):
             if m.group(1) or m.group(2):
                 quoted = m.group(0)
@@ -23,27 +26,43 @@ def eval(cmdline: str, out: deque) -> None:
                 #     tokens.extend(globbing)
                 # else:
                 tokens.append(m.group(0))
+
         app = ApplicationFactory.by_name(tokens[0])
         args = tokens[1:]
         app.exec(args, None, out)
 
 
-if __name__ == "__main__":
-    args_num = len(sys.argv) - 1
-    if args_num > 0:
-        if args_num != 2:
-            raise ValueError("wrong number of command line arguments")
-        if sys.argv[1] != "-c":
-            raise ValueError(f"unexpected command line argument {sys.argv[1]}")
-        out = deque()
-        eval(sys.argv[2], out)
-        while len(out) > 0:
-            print(out.popleft(), end="")
+def run(cmdline: str, out: deque) -> None:
+    eval(cmdline, out)
+
+    while len(out) > 0:
+        print(out.popleft(), end="")
+
+
+def non_interactive_mode(num_args: int) -> None:
+    if num_args != 2:
+        raise ValueError("wrong number of command line arguments")
+
+    if sys.argv[1] != "-c":
+        raise ValueError(f"unexpected command line argument {sys.argv[1]}")
+
+    run(sys.argv[2], deque())
+
+
+def interactive_mode() -> None:
+    while True:
+        print(os.getcwd() + "> ", end="")
+        run(input(), deque())
+
+
+def main() -> None:
+    num_args = len(sys.argv) - 1
+
+    if num_args > 0:
+        non_interactive_mode(num_args)
     else:
-        while True:
-            print(os.getcwd() + "> ", end="")
-            cmdline = input()
-            out = deque()
-            eval(cmdline, out)
-            while len(out) > 0:
-                print(out.popleft(), end="")
+        interactive_mode()
+
+
+if __name__ == "__main__":
+    main()
