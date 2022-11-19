4ffb507cccbc42daf8bbe7fcda74dc9e83e65354
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-11-19 18:32:22 +0000

    Minor fixes

--- src/commands/impl/pipe.py
+++ src/commands/impl/pipe.py
@@ -6,10 +6,10 @@ from typing import Optional
 
 class Pipe(Command):
     def __init__(self, left: Command, right: Call) -> None:
-        self.left = left
-        self.right = right
+        self.__left = left
+        self.__right = right
 
     def eval(self, input_: Optional[str], out: deque) -> None:
         temp_out = deque()
-        self.left.eval(input_, temp_out)
-        self.right.eval("".join(temp_out), out)
+        self.__left.eval(input_, temp_out)
+        self.__right.eval("".join(temp_out), out)
--- src/commands/impl/seq.py
+++ src/commands/impl/seq.py
@@ -5,9 +5,9 @@ from typing import Optional
 
 class Seq(Command):
     def __init__(self, left: Command, right: Command) -> None:
-        self.left = left
-        self.right = right
+        self.__left = left
+        self.__right = right
 
     def eval(self, input_: Optional[str], out: deque) -> None:
-        self.left.eval(input_, out)
-        self.right.eval(None, out)
+        self.__left.eval(input_, out)
+        self.__right.eval(None, out)
