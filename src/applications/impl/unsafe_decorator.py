d2b0083aacf625ed9c6e503a0bf1ffe629c7e227
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-11-23 11:41:09 +0000

    Add more data classes

--- src/applications/impl/unsafe_decorator.py
+++ src/applications/impl/unsafe_decorator.py
@@ -1,14 +1,15 @@
 from ..application import Application
 from collections import deque
+from dataclasses import dataclass
 from typing import Optional
 
 
+@dataclass
 class UnsafeDecorator(Application):
-    def __init__(self, app: Application) -> None:
-        self.__app = app
+    application: Application
 
     def exec(self, args: list, input_: Optional[list], out: deque) -> None:
         try:
-            self.__app.exec(args, input_, out)
+            self.application.exec(args, input_, out)
         except Exception as exception:
             out.append(f"{exception}\n")
