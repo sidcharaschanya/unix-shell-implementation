6e6b613f3d6f27fe44726123b09de0be848c906a
aadi singhi <aadi.singhi.21@ucl.ac.uk>
2022-11-24 14:57:43 +0000

    test double quote with single quotes

--- test/test_commands/test_command_visitor.py
+++ test/test_commands/test_command_visitor.py
@@ -1,56 +1,76 @@
+import os
+import shutil
 import unittest
+from collections import deque
 
 from commands.command_visitor import CommandVisitor
 from commands.impl.seq import Seq
 from commands.impl.call import Call
 from commands.impl.pipe import Pipe
 
-
 class TestCommandVisitor(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.out = deque()
+        self.temp_dir = "resources"
+        os.mkdir(self.temp_dir)
+        self.paths = dict()
+
+        self.files = {
+            "test1.txt": "hello\n",
+            #"test2.txt": "hehehehe\nhellohello\n",
+            #"empty_file.txt": ""
+        }
+
+        for file_name, file_content in self.files.items():
+            with open(os.path.join(self.temp_dir, file_name), "w") as file:
+                file.write(file_content)
+                self.paths[file_name] = file.name
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.temp_dir)
+
     def test_redirection_output(self):
-        cmdline = "echo hello > test1.txt"
+        cmdline = "echo hello > text1.txt"
         command = CommandVisitor.parse(cmdline)
-        expected = Call("echo", ["hello"], None, "test1.txt")
+        expected = Call("echo", ["hello"], None,"text1.txt")
         self.assertEqual(command, expected)
 
     def test_redirection_input(self):
-        cmdline = "cat < test1.txt"
+        cmdline = "cat < text1.txt"
         command = CommandVisitor.parse(cmdline)
-        expected = Call("cat", [], "test1.txt", None)
+        expected = Call("cat", [], "text1.txt", None)
         self.assertEqual(command, expected)
 
     def test_seq_command(self):
-        cmdline = "echo hello;echo hi"
-        command = CommandVisitor.parse(cmdline)
-        expected = Seq(Call("echo", ["hello"], None, None), Call("echo", ["hi"], None, None))
-        self.assertEqual(command, expected)
+            cmdline="echo hello;echo hi"
+            command=CommandVisitor.parse(cmdline)
+            expected=Seq(Call("echo",["hello"],None,None),Call("echo",["hi"],None,None))
+            self.assertEqual(command,expected)
 
     def test_nested_seq(self):
-        cmdline = "echo hello; echo hi; cat test1.txt"
-        command = CommandVisitor.parse(cmdline)
-        expected = Seq(Seq(Call("echo", ["hello"], None, None), Call("echo", ["hi"], None, None)),
-                       Call("cat", ["test1.txt"], None, None))
+        cmdline="echo hello; echo hi; cat test.txt"
+        command=CommandVisitor.parse(cmdline)
+        expected=Seq(Seq(Call("echo",["hello"],None,None),Call("echo",["hi"],None,None)),Call("cat",["test.txt"],None,None))
         self.assertEqual(command, expected)
 
     def test_pipe(self):
-        cmdline = "cat test1.txt | grep 'Interesting String'"
-        command = CommandVisitor.parse(cmdline)
-        expected = Pipe(Call("cat", ["test1.txt"], None, None), Call("grep", ["Interesting String"], None, None))
+        cmdline="cat test.txt | grep 'Interesting String'"
+        command=CommandVisitor.parse(cmdline)
+        expected=Pipe(Call("cat",["test.txt"],None,None),Call("grep",["Interesting String"],None,None))
         self.assertEqual(command, expected)
 
     def test_nested_pipe(self):
-        cmdline = "echo 'Interesting String' | grep 'Interesting String' | grep 'Ie'"
-        command = CommandVisitor.parse(cmdline)
-        expected = Pipe(
-            Pipe(Call("echo", ['Interesting String'], None, None), Call("grep", ["Interesting String"], None, None)),
-            Call("grep", ["Ie"], None, None))
+        cmdline="echo 'Interesting String' | grep 'Interesting String' | grep 'Ie'"
+        command=CommandVisitor.parse(cmdline)
+        expected=Pipe(Pipe(Call("echo",['Interesting String'],None,None),Call("grep",["Interesting String"],None,None)),Call("grep",["Ie"],None,None))
         self.assertEqual(command, expected)
 
     def test_single_quote(self):
         cmdline = "echo 'Interesting String'"
         command = CommandVisitor.parse(cmdline)
-        expected = Call("echo", ['Interesting String'], None, None)
-        self.assertEqual(command, expected)
+        expected = Call("echo",['Interesting String'],None,None)
+        self.assertEqual(command,expected)
 
     def test_single_quote_with_back_quotes(self):
         cmdline = "echo 'hello `echo a`'"
@@ -70,11 +90,11 @@ class TestCommandVisitor(unittest.TestCase):
         expected = Call("echo", ['hello a '], None, None)
         self.assertEqual(command, expected)
 
-    def test_back_quotes(self):
-        pass
-
     def test_unquoted(self):
         cmdline = "echo hello"
         command = CommandVisitor.parse(cmdline)
-        expected = Call("echo", ['hello'], None, None)
-        self.assertEqual(command, expected)
+        expected = Call("echo",['hello'],None,None)
+        self.assertEqual(command,expected)
+
+
+
