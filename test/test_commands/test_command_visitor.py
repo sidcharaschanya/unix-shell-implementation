6ddc4e91d6541232ceb01cdf3d22ad5c369f33fa
aadi singhi <aadi.singhi.21@ucl.ac.uk>
2022-11-24 00:49:23 +0000

    Setup temp directory

--- test/test_commands/test_command_visitor.py
+++ test/test_commands/test_command_visitor.py
@@ -1,4 +1,8 @@
+import os
+import shutil
 import unittest
+from collections import deque
+
 from commands.command_visitor import CommandVisitor
 from commands.impl.seq import Seq
 from commands.impl.call import Call
@@ -6,11 +10,31 @@ from commands.impl.pipe import Pipe
 
 class TestCommandVisitor(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.out = deque()
+        self.temp_dir = "resources"
+        os.mkdir(self.temp_dir)
+        self.paths = dict()
+
+        self.files = {
+            "test1.txt": "hello\n",
+            #"test2.txt": "hehehehe\nhellohello\n",
+            #"empty_file.txt": ""
+        }
+
+        for file_name, file_content in self.files.items():
+            with open(os.path.join(self.temp_dir, file_name), "w") as file:
+                file.write(file_content)
+                self.paths[file_name] = file.name
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.temp_dir)
+
     def test_seq_command(self):
-        cmdline="echo hello;echo hi"
-        command=CommandVisitor.parse(cmdline)
-        expected=Seq(Call("echo",["hello"],None,None),Call("echo",["hi"],None,None))
-        self.assertEqual(command,expected)
+            cmdline="echo hello;echo hi"
+            command=CommandVisitor.parse(cmdline)
+            expected=Seq(Call("echo",["hello"],None,None),Call("echo",["hi"],None,None))
+            self.assertEqual(command,expected)
 
     def test_nested_seq(self):
         cmdline="echo hello; echo hi; cat test.txt"
@@ -59,4 +83,13 @@ class TestCommandVisitor(unittest.TestCase):
 
     def test_back_quotes(self):
         pass
+
+    def test_unquoted(self):
+        cmdline = "echo hello"
+        command = CommandVisitor.parse(cmdline)
+        expected = Call("echo",['hello'],None,None)
+        self.assertEqual(command,expected)
+
+    #def test_
+
 
