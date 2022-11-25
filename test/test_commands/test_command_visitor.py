2048a4650671a23f7b7db3e188f1c959981e6f6b
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-11-25 23:28:35 +0000

    Minor fixes

--- test/test_commands/test_command_visitor.py
+++ test/test_commands/test_command_visitor.py
@@ -1,11 +1,11 @@
 import unittest
 
+from antlr4.error.Errors import ParseCancellationException
 from commands.command_visitor import CommandVisitor
+from commands.exceptions.redirection_error import RedirectionError
 from commands.impl.call import Call
 from commands.impl.pipe import Pipe
 from commands.impl.seq import Seq
-from antlr4.error.Errors import ParseCancellationException
-from commands.exceptions.redirection_error import RedirectionError
 
 
 class TestCommandVisitor(unittest.TestCase):
@@ -79,26 +79,27 @@ class TestCommandVisitor(unittest.TestCase):
         self.assertEqual(command, expected)
 
     def test_wrong_command(self):
-        cmdline="echo '"
+        cmdline = "echo '"
         with self.assertRaises(ParseCancellationException):
             CommandVisitor.parse(cmdline)
 
     def test_several_redirection_files(self):
-        cmdline="echo < `echo test1.txt test2.txt`"
+        cmdline = "echo < `echo test1.txt test2.txt`"
         with self.assertRaises(RedirectionError):
             CommandVisitor.parse(cmdline)
 
     def test_several_input_redirection_files(self):
-        cmdline="echo < test1.txt < test2.txt"
+        cmdline = "echo < test1.txt < test2.txt"
         with self.assertRaises(RedirectionError):
             CommandVisitor.parse(cmdline)
 
     def test_several_output_redirection_files(self):
-        cmdline="echo 'hello' > test1.txt > test2.txt"
+        cmdline = "echo 'hello' > test1.txt > test2.txt"
         with self.assertRaises(RedirectionError):
             CommandVisitor.parse(cmdline)
 
     def test_call_with_redirections(self):
-        cmdline="<test.txt echo"
-        command=CommandVisitor.parse(cmdline)
-        expected=Call("echo",[],"text.txt",None)
+        cmdline = "<test.txt echo"
+        command = CommandVisitor.parse(cmdline)
+        expected = Call("echo", [], "text.txt", None)
+        self.assertEqual(command, expected)
